25d9dc922d3d05fb7722af2b525ec00ddcf63f37
21pd38 <VamsDeward987@gmail.com>
2024-03-08 09:38:14 +0530

    modified main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Start")
+from flask import Flask, render_template, request, redirect, url_for, jsonify, flash
+import mysql.connector
+from flask_session import Session
+
+mydb = mysql.connector.connect(
+    host='127.0.0.1',  
+    user='root',  
+    password='VamsKris@987',
+    database='foxsense'
+)
+
+mycursor = mydb.cursor(buffered=True)
